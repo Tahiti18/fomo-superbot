d9fef9d6886fbf6ee78f1f5051212f1aa041ced7
Tahiti18 <35585881+Tahiti18@users.noreply.github.com>
2025-08-29 23:22:09 +0300

    Update billing.ts

--- src/handlers/billing.ts
+++ src/handlers/billing.ts
@@ -1,6 +1,6 @@
 // src/handlers/billing.ts
 import type { Context } from "grammy";
-import { createInvoice } from "../payments/cryptoPay.js";
+import { createInvoice } from "../payments/cryptoPay";
 import { InlineKeyboard } from "grammy";
 
 type Plan = "starter" | "pro" | "lifetime";
